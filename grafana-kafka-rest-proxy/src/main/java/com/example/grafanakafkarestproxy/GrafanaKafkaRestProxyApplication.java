1c55272e062a6b547bd641ac4148d0eefd4bb040
KalanaGeesara <kalanageesara.15@cse.mrt.ac.lk>
2021-12-24 21:53:26 +0530

    initial commit

---
+++ grafana-kafka-rest-proxy/src/main/java/com/example/grafanakafkarestproxy/GrafanaKafkaRestProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.grafanakafkarestproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaKafkaRestProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaKafkaRestProxyApplication.class, args);
+	}
+
+}
---
+++ grafana-kafka-rest-proxy/src/test/java/com/example/grafanakafkarestproxy/GrafanaKafkaRestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafanakafkarestproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaKafkaRestProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
